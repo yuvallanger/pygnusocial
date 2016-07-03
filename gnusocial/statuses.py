1149a01c6f89488f64a9602a46153fcca8ff807c
dtluna <dtluna@openmailbox.org>
2016-07-03 12:46:46 +0300

    Added authentication to public_timeline()

--- gnusocial/statuses.py
+++ gnusocial/statuses.py
@@ -1,8 +1,15 @@
 from .utils import _get_request
 
 
-def public_timeline(server_url: str) -> list:
-    return _get_request(server_url, 'statuses/public_timeline')
+def public_timeline(server_url: str,
+                    username: str = '',
+                    password: str = '') -> list:
+    if username:
+        return _get_request(server_url,
+                            'statuses/public_timeline',
+                            (username, password))
+    else:
+        return _get_request(server_url, 'statuses/public_timeline')
 
 
 def home_timeline(server_url: str, username: str, password: str) -> list:
