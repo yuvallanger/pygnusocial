fc11f6be6c5997ee8844e3f32002c4e01d7f8538
dtluna <dtluna@openmailbox.org>
2016-07-03 12:01:56 +0300

    Added home_timeline()

--- gnusocial/statuses.py
+++ gnusocial/statuses.py
@@ -1,5 +1,11 @@
-from gnusocial import _get_request
+from .utils import _get_request
 
 
 def public_timeline(server_url: str) -> list:
     return _get_request(server_url, 'statuses/public_timeline')
+
+
+def home_timeline(server_url: str, username: str, password: str) -> list:
+    return _get_request(server_url,
+                        'statuses/home_timeline',
+                        (username, password))
