1e783e1d0d92c5d7b0a6d37e51eb669797554fa5
dtluna <dtluna@openmailbox.org>
2016-07-04 15:33:24 +0300

    Added timeline.user()

--- gnusocial/timeline.py
+++ gnusocial/timeline.py
@@ -20,3 +20,16 @@ def friends(server_url: str, username: str, password: str) -> dict:
     return _get_request(server_url,
                         'statuses/friends_timeline',
                         (username, password))
+
+
+def user(server_url: str,
+         username: str,
+         password: str,
+         target_user: str) -> dict:
+    if username:
+        return _get_request(server_url,
+                            'statuses/friends_timeline' + target_user,
+                            (username, password))
+    else:
+        return _get_request(server_url,
+                            'statuses/friends_timeline/' + target_user)
