ef69ffd0bb6d996863c89c8e7098f8287f3beb00
dtluna <dtluna@openmailbox.org>
2016-07-12 00:52:07 +0300

    Added groups.show()

--- gnusocial/groups.py
+++ gnusocial/groups.py
@@ -1,11 +1,11 @@
-from .utils import _check_id_and_nickname, _post_request
+from .utils import _check_id_and_nickname, _post_request, _get_request
 
 
 def _resource_path(resource_path: str, **kwargs):
     if 'id' in kwargs:
-        resource_path += '%d' % kwargs['id']
+        resource_path += '/%d' % kwargs['id']
     elif 'nickname' in kwargs:
-        resource_path += '%s' % kwargs['nickname']
+        resource_path += '/%s' % kwargs['nickname']
     else:
         raise Exception(
             "You must either specify the id or nickname."
@@ -18,9 +18,21 @@ def timeline(server_url: str,
              password: str='',
              **kwargs) -> list:
     _check_id_and_nickname(**kwargs)
-    resource_path = _resource_path('statusnet/groups/timeline/', **kwargs)
+    resource_path = _resource_path('statusnet/groups/timeline', **kwargs)
     return _post_request(server_url=server_url,
                          resource_path=resource_path,
                          username=username,
                          password=password,
                          data=kwargs)
+
+
+def show(server_url: str,
+         username: str='',
+         password: str='',
+         **kwargs) -> dict:
+    _check_id_and_nickname(**kwargs)
+    resource_path = _resource_path('statusnet/groups/show', **kwargs)
+    return _get_request(server_url=server_url,
+                        resource_path=resource_path,
+                        username=username,
+                        password=password)
