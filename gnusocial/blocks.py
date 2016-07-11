3a376d83bdff34a60798c24421ebd52ccc2c0cb4
dtluna <dtluna@openmailbox.org>
2016-07-11 19:15:52 +0300

    Added blocks.create()

---
+++ gnusocial/blocks.py
@@ -0,0 +1,13 @@
+from .utils import _post_request, _check_user_id_and_screen_name
+
+
+def create(server_url: str,
+           username: str,
+           password: str,
+           **kwargs) -> dict:
+    _check_user_id_and_screen_name(**kwargs)
+    return _post_request(server_url=server_url,
+                         resource_path='blocks/create',
+                         username=username,
+                         password=password,
+                         data=kwargs)
