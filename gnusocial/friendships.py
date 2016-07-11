3caa43551c9ac95c4866131c12b6acfcaf45f899
dtluna <dtluna@openmailbox.org>
2016-07-11 16:54:50 +0300

    Added friedships.show()

--- gnusocial/friendships.py
+++ gnusocial/friendships.py
@@ -33,3 +33,32 @@ def exists(server_url: str,
                          username=username,
                          password=password,
                          data={'user_a': user_a, 'user_b': user_b})
+
+
+def show(server_url: str,
+         username: str='',
+         password: str='',
+         **kwargs) -> dict:
+    both_targets = 'target_id' in kwargs and 'target_screen_name' in kwargs
+    no_targets = 'target_id' not in kwargs and \
+        'target_screen_name' not in kwargs
+    if both_targets or no_targets:
+        raise Exception(
+            "You must either specify target_id or target_screen_name."
+        )
+    both_sources = 'source_id' in kwargs and 'source_screen_name' in kwargs
+    no_sources = 'source_id' not in kwargs and \
+        'source_screen_name' not in kwargs
+    if both_sources:
+        raise Exception(
+            "You must either specify source_id or source_screen_name."
+        )
+    if no_sources and not username:
+        raise Exception(
+            "You must either specify source_id or " +
+            "source_screen_name or username")
+    return _post_request(server_url=server_url,
+                         resource_path='friendships/show',
+                         username=username,
+                         password=password,
+                         data=kwargs)
