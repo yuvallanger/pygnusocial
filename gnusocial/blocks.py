082284ee7996caf0b2134b38850d733b17365369
dtluna <dtluna@openmailbox.org>
2016-07-11 19:19:12 +0300

    Added blocks.destroy()

--- gnusocial/blocks.py
+++ gnusocial/blocks.py
@@ -11,3 +11,15 @@ def create(server_url: str,
                          username=username,
                          password=password,
                          data=kwargs)
+
+
+def destroy(server_url: str,
+            username: str,
+            password,
+            **kwargs) -> dict:
+    _check_user_id_and_screen_name(**kwargs)
+    return _post_request(server_url=server_url,
+                         resource_path='blocks/destroy',
+                         username=username,
+                         password=password,
+                         data=kwargs)
