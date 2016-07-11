acb0cdac5d5b139f3c8be4b6c0122c3aeb6432cb
dtluna <dtluna@openmailbox.org>
2016-07-11 15:39:52 +0300

    Added friendships.exists()

--- gnusocial/friendships.py
+++ gnusocial/friendships.py
@@ -21,3 +21,15 @@ def destroy(server_url: str,
                          username,
                          password,
                          {'id': target_user})
+
+
+def exists(server_url: str,
+           user_a: str,
+           user_b: str,
+           username: str='',
+           password: str='') -> dict:
+    return _post_request(server_url=server_url,
+                         resource_path='friendships/exists',
+                         username=username,
+                         password=password,
+                         data={'user_a': user_a, 'user_b': user_b})
