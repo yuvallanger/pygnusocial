fb18ec005ef3e4669fb9b1d6af212458a1219b4c
dtluna <dtluna@openmailbox.org>
2016-07-11 17:46:29 +0300

    Added friends.followers()

--- gnusocial/friends.py
+++ gnusocial/friends.py
@@ -1,10 +1,7 @@
 from .utils import _post_request
 
 
-def friends(server_url: str,
-            username: str='',
-            password: str='',
-            **kwargs) -> list:
+def _check_args(username: str='', **kwargs):
     both_targets = 'user_id' in kwargs and 'screen_name' in kwargs
     no_targets = 'user_id' not in kwargs and 'screen_name' not in kwargs
     if both_targets:
@@ -16,8 +13,27 @@ def friends(server_url: str,
             "You must either specify the user_id or screen_name or " +
             "username."
         )
+
+
+def friends(server_url: str,
+            username: str='',
+            password: str='',
+            **kwargs) -> list:
+    _check_args(username, **kwargs)
     return _post_request(server_url=server_url,
                          resource_path='friends/ids',
+                         username=username,
+                         password=password,
+                         data=kwargs)
+
+
+def followers(server_url: str,
+              username: str='',
+              password: str='',
+              **kwargs) -> list:
+    _check_args(username, **kwargs)
+    return _post_request(server_url=server_url,
+                         resource_path='followers/ids',
                          username=username,
                          password=password,
                          data=kwargs)
