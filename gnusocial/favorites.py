7de30677d0a2ff48e0f5d5f9327e8628d1988786
dtluna <dtluna@openmailbox.org>
2016-07-11 19:02:15 +0300

    Added favorites.create()

--- gnusocial/favorites.py
+++ gnusocial/favorites.py
@@ -1,7 +1,7 @@
-from .utils import _get_request, _post_request
+from .utils import _post_request
 
 
-def _check_args(**kwargs):
+def _check_args(**kwargs) -> None:
     both_targets = 'user_id' in kwargs and 'screen_name' in kwargs
     if both_targets:
         raise Exception(
@@ -19,3 +19,13 @@ def favorites(server_url: str,
                          username=username,
                          password=password,
                          data=kwargs)
+
+
+def create(server_url: str,
+           username: str,
+           password: str,
+           post_id: int) -> dict:
+    return _post_request(server_url=server_url,
+                         resource_path='favorites/create/%d' % post_id,
+                         username=username,
+                         password=password)
