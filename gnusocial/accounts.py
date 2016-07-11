8e6be7f4a021646f9d3248effa5d76d250581095
dtluna <dtluna@openmailbox.org>
2016-07-11 18:33:24 +0300

    Removed redundant indentation.

--- gnusocial/accounts.py
+++ gnusocial/accounts.py
@@ -1,10 +1,8 @@
 from .utils import _get_request
 
 
-def verify_credentials(server_url: str,
-                       username: str,
-                       password: str) -> None:
-    _get_request(
+def verify_credentials(server_url: str, username: str, password: str) -> dict:
+    return _get_request(
         server_url=server_url,
         resource_path='account/verify_credentials',
         username=username,
