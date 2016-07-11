95ba8413a5bcccffe236a8a8f2d7f80840e01cce
dtluna <dtluna@openmailbox.org>
2016-07-11 22:12:45 +0300

    Added search.search()

---
+++ gnusocial/search.py
@@ -0,0 +1,12 @@
+from .utils import _get_request
+
+
+def search(server_url: str,
+           query: str,
+           username: str='',
+           password: str='') -> list:
+    return _get_request(server_url=server_url,
+                        resource_path='search.json?q=%s' % query,
+                        username=username,
+                        password=password,
+                        extension='')
