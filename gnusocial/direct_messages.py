e290008bdb1937bc9705bf0d47df688a010cd084
dtluna <dtluna@openmailbox.org>
2016-07-11 13:34:48 +0300

    Implemented direct_messages resources.

---
+++ gnusocial/direct_messages.py
@@ -0,0 +1,22 @@
+from .utils import _get_request, _post_request
+
+
+def received(server_url: str, username: str, password: str) -> list:
+    return _get_request(server_url, 'direct_messages', username, password)
+
+
+def sent(server_url: str, username: str, password: str) -> list:
+    return _get_request(server_url, 'direct_messages/sent', username, password)
+
+
+def new(server_url: str,
+        username: str,
+        password: str,
+        target_user: str,
+        text: str) -> list:
+    data = {'user': target_user, 'text': text}
+    return _post_request(server_url,
+                         'direct_messages/new',
+                         username,
+                         password,
+                         data=data)
