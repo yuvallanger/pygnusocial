9f8b49a09b5fc9efaec24145d45f7edaac4bb90c
dtluna <dtluna@openmailbox.org>
2016-07-13 20:53:33 +0300

    Refactored groups._resource_path()

--- gnusocial/groups.py
+++ gnusocial/groups.py
@@ -1,15 +1,17 @@
-from .utils import _check_id_and_nickname, _post_request, _get_request
+from .utils import _post_request, _get_request
 
 
 def _resource_path(resource_path: str, **kwargs):
-    if 'id' in kwargs:
-        resource_path += '/%d' % kwargs['id']
-    elif 'nickname' in kwargs:
-        resource_path += '/%s' % kwargs['nickname']
-    else:
+    group_id = kwargs.get('id')
+    group_name = kwargs.get('nickname')
+    if bool(group_id) == bool(group_name):
         raise Exception(
             "You must either specify the id or nickname."
         )
+    if group_id:
+        resource_path += '/%d' % group_id
+    if group_name:
+        resource_path += '/%s' % group_name
     return resource_path
 
 
@@ -17,7 +19,6 @@ def timeline(server_url: str,
              username: str='',
              password: str='',
              **kwargs) -> list:
-    _check_id_and_nickname(**kwargs)
     resource_path = _resource_path('statusnet/groups/timeline', **kwargs)
     return _post_request(server_url=server_url,
                          resource_path=resource_path,
@@ -30,7 +31,6 @@ def show(server_url: str,
          username: str='',
          password: str='',
          **kwargs) -> dict:
-    _check_id_and_nickname(**kwargs)
     resource_path = _resource_path('statusnet/groups/show', **kwargs)
     return _get_request(server_url=server_url,
                         resource_path=resource_path,
