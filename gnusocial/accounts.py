be60c3ee89b7bf8d4189970c1ff2f5a753df6652
dtluna <dtluna@openmailbox.org>
2016-07-19 20:09:43 +0300

    Added accounts.update_profile_image()

--- gnusocial/accounts.py
+++ gnusocial/accounts.py
@@ -1,10 +1,22 @@
-from .utils import _get_request
+from .utils import _get_request, _post_request
 
 
 def verify_credentials(server_url: str, username: str, password: str) -> dict:
-    _get_request(
+    return _get_request(
         server_url=server_url,
         resource_path='account/verify_credentials',
         username=username,
         password=password
-    )
+    ).json()
+
+
+def update_profile_image(server_url: str,
+                         username: str,
+                         password: str,
+                         filename: str) -> dict:
+    image = {'image': open(filename, 'rb')}
+    return _post_request(server_url=server_url,
+                         resource_path='account/update_profile_image',
+                         username=username,
+                         password=password,
+                         media=image)
