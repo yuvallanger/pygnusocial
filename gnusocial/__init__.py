5fe9c3a17f84594caf372d6940bb3e66cd3a5c73
dtluna <dtluna@openmailbox.org>
2016-07-20 12:54:53 +0300

    Added module list to __init__.py

---
+++ gnusocial/__init__.py
@@ -0,0 +1,21 @@
+"""
+pygnusocial - Python API for GNU Social
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+pygnusocial is a library to make it easy to interact with GNU Social API.
+
+Modules:
+    accounts
+    blocks
+    direct_messages
+    favorites
+    friends
+    friendships
+    groups
+    media
+    search
+    statuses
+    timelines
+    users
+    utils
+"""
