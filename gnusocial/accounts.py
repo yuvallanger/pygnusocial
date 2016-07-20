903a0fcbb4c843c46bacd00990c67950c47733de
dtluna <dtluna@openmailbox.org>
2016-07-20 11:43:58 +0300

    Removed return value from accounts.verify_credentials()

--- gnusocial/accounts.py
+++ gnusocial/accounts.py
@@ -2,12 +2,12 @@ from .utils import _get_request, _post_request
 
 
 def verify_credentials(server_url: str, username: str, password: str) -> dict:
-    return _get_request(
+    _get_request(
         server_url=server_url,
         resource_path='account/verify_credentials',
         username=username,
         password=password
-    ).json()
+    )
 
 
 def update_profile_image(server_url: str,
