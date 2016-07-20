1bd3f7469e1fbcceb92bcba37b7d58e92abdfc54
dtluna <dtluna@openmailbox.org>
2016-07-20 20:52:31 +0300

    Added dosctrings to direct_messages module.

--- gnusocial/direct_messages.py
+++ gnusocial/direct_messages.py
@@ -1,7 +1,42 @@
-from .utils import _get_request, _post_request
+"""
+gnusocial.direct_messages
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Module with direct messages resources.
+"""
+from .utils import _get_request, _post_request, _check_user_id_and_screen_name
 
 
 def received(server_url: str, username: str, password: str, **kwargs) -> list:
+    """Returns the 20 most recent direct messages sent to the authenticating
+    user. Includes detailed information about the sender and recipient user.
+    You can request up to 200 direct messages per call, and only the most
+    recent 200 DMs will be available using this endpoint.
+
+    :param server_url: URL of the server
+    :param username: name of the authenticating user
+    :param password: password of the authenticating user
+    :param since_id: (optional) Returns results with an ID greater than
+        (that is, more recent than) the specified ID.
+    :param max_id: (optional) Returns results with an ID less than
+        (that is, older than) or equal to the specified ID.
+    :param count: (optional) Specifies the number of direct messages to try
+        and retrieve, up to a maximum of 200.
+    :param include_entities: (optional) The entities node will not be included
+        when set to false.
+    :param skip_status: (optional) When set to either True or 1 statuses
+        will not be included in the returned user objects.
+    :return: list of dicts with following structure:
+        created_at: - date of message creation
+        id
+        recipient - user info dict
+        recipient_id
+        recipient_screen_name
+        sender - user info dict
+        sender_id
+        sender_screen_name
+        text
+    """
     return _get_request(server_url=server_url,
                         resource_path='direct_messages',
                         username=username,
@@ -9,21 +44,74 @@ def received(server_url: str, username: str, password: str, **kwargs) -> list:
                         params=kwargs).json()
 
 
-def sent(server_url: str, username: str, password: str) -> list:
+def sent(server_url: str, username: str, password: str, **kwargs) -> list:
+    """Returns the 20 most recent direct messages sent by the authenticating
+    user. Includes detailed information about the sender and recipient user.
+    You can request up to 200 direct messages per call, and only the most
+    recent 200 DMs will be available using this endpoint.
+
+    :param server_url: URL of the server
+    :param username: name of the authenticating user
+    :param password: password of the authenticating user
+    :param since_id: (optional) Returns results with an ID greater than
+        (that is, more recent than) the specified ID.
+    :param max_id: (optional) Returns results with an ID less than
+        (that is, older than) or equal to the specified ID.
+    :param count: (optional) Specifies the number of direct messages to try
+        and retrieve, up to a maximum of 200.
+    :param include_entities: (optional) The entities node will not be included
+        when set to false.
+    :param skip_status: (optional) When set to either True or 1 statuses
+        will not be included in the returned user objects.
+    :return: list of dicts with following structure:
+        created_at: - date of message creation
+        id
+        recipient - user info dict
+        recipient_id
+        recipient_screen_name
+        sender - user info dict
+        sender_id
+        sender_screen_name
+        text
+    """
     return _get_request(server_url=server_url,
                         resource_path='direct_messages/sent',
                         username=username,
-                        password=password).json()
+                        password=password,
+                        params=kwargs).json()
 
 
 def new(server_url: str,
         username: str,
         password: str,
-        target_user: str,
-        text: str) -> list:
-    data = {'user': target_user, 'text': text}
+        text: str,
+        **kwargs) -> dict:
+    """ Sends a new direct message to the specified user from
+    the authenticating user.
+
+    :param server_url: URL of the server
+    :param username: name of the authenticating user
+    :param password: password of the authenticating user
+    :param text: The text of your direct message.
+    :param user_id: (optional) The ID of the user who should receive the
+        direct message.
+    :param screen_name: (optional) The screen name of the user who should
+        receive the direct message.
+    :return: dict with following structure:
+        created_at: - date of message creation
+        id
+        recipient - user info dict
+        recipient_id
+        recipient_screen_name
+        sender - user info dict
+        sender_id
+        sender_screen_name
+        text
+    """
+    _check_user_id_and_screen_name(**kwargs)
+    kwargs['text'] = text
     return _post_request(server_url=server_url,
                          resource_path='direct_messages/new',
                          username=username,
                          password=password,
-                         data=data).json()
+                         data=kwargs).json()
