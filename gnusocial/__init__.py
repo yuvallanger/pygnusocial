44a8ba57d9b8e11dec881274fb6d63a1ad81c044
dtluna <dtluna@openmailbox.org>
2016-07-20 13:50:36 +0300

    Removed list, because help() gives contents already.

--- gnusocial/__init__.py
+++ gnusocial/__init__.py
@@ -3,19 +3,4 @@ pygnusocial - Python API for GNU Social
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 pygnusocial is a library to make it easy to interact with GNU Social API.
-
-Modules:
-    accounts
-    blocks
-    direct_messages
-    favorites
-    friends
-    friendships
-    groups
-    media
-    search
-    statuses
-    timelines
-    users
-    utils
 """
