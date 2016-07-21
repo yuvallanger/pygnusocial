132ab705b992f5dc1a0b55e45ae75509fb939704
dtluna <dtluna@openmailbox.org>
2016-07-22 02:54:36 +0300

    Added docstrings to search module

--- gnusocial/search.py
+++ gnusocial/search.py
@@ -1,3 +1,9 @@
+"""
+gnusocial.search
+~~~~~~~~~~~~~~~~
+
+Module with search resources.
+"""
 from .utils import _get_request
 
 
@@ -5,6 +11,14 @@ def search(server_url: str,
            query: str,
            username: str='',
            password: str='') -> list:
+    """Returns a collection of statuses matching a specified query.
+
+    :param server_url: URL of the server
+    :param query: UTF-8 encoded query
+    :param username: (optional) name of the authenticating user
+    :param password: (optional) password of the authenticating user
+    :return: list of post dicts.
+    """
     return _get_request(server_url=server_url,
                         resource_path='search.json?q=%s' % query,
                         username=username,
