4023ae113c92ad76801d51019551c1b902d903f0
dtluna <dtluna@openmailbox.org>
2016-08-09 01:15:31 +0300

    Added statuses.conversation()

--- gnusocial/statuses.py
+++ gnusocial/statuses.py
@@ -7,7 +7,7 @@ Module with status resources.
 from dtd import docstring
 from .utils import _post_request, _get_request
 from .docs import (_SERVER_URL_DOC, _USERNAME_DOC, _PASSWORD_DOC, _STATUS_DICT,
-                   _STATUS_ID_DOC)
+                   _STATUS_ID_DOC, _STATUSES_COUNT, _SINCE_ID_DOC, _MAX_ID_DOC)
 
 
 @docstring(server_url=_SERVER_URL_DOC,
@@ -143,3 +143,39 @@ def repeat(server_url: str,
                          resource_path='statuses/retweet/%d' % status_id,
                          username=username,
                          password=password).json()
+
+
+@docstring(server_url=_SERVER_URL_DOC,
+           username=_USERNAME_DOC,
+           password=_PASSWORD_DOC,
+           since_id=_SINCE_ID_DOC,
+           max_id=_MAX_ID_DOC,
+           count=_STATUSES_COUNT,
+           status_dict=_STATUS_DICT)
+def conversation(server_url: str,
+                 conversation_id: int,
+                 username: str='',
+                 password: str='',
+                 **kwargs) -> list:
+    """Returns statuses that have been posted in the conversation.
+
+:param str server_url: {server_url}
+:param int conversation_id: The ID of the conversation.
+:param str username: (optional) {username}
+:param str password: (optional) {password}
+:param int since_id: (optional) {since_id}
+:param int max_id: (optional) {max_id}
+:param int count: (optional) {count}
+:rtype: list
+:return: list of dicts with following structure:
+
+::
+
+    {status_dict}
+    """
+    resource_path = 'statusnet/conversation/%d' % conversation_id
+    return _get_request(server_url=server_url,
+                        resource_path=resource_path,
+                        username=username,
+                        password=password,
+                        params=kwargs).json()
